06221f8316489e9289ef8a140dbf35ab7c861d67
Jean Lapostolle <lapostolle.jean@gmail.com>
2018-03-28 16:05:35 +0200

    begin project

---
+++ cmake-build-debug/CMakeFiles/shell_lapostolle_saby.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell_lapostolle_saby.dir/main.cpp.o"
+  "shell_lapostolle_saby.pdb"
+  "shell_lapostolle_saby"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shell_lapostolle_saby.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/shell_lapostolle_saby.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shell_lapostolle_saby.
+# This may be replaced when dependencies are built.
